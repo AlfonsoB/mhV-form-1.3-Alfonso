571d5cc39d4be74b073469c5ccd3446a2b0f61e8
AlfonsoB <AlfonsoB@users.noreply.github.com>
2023-04-16 22:44:43 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# mhV-form-1.3-Alfonso
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ts-in2hig)
---
+++ index.tsx
@@ -0,0 +1,32 @@
+import * as React from 'react';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from './homePage';
+import NavBar from './navBar';
+import Footer from './footer';
+import { BrowserRouter } from 'react-router-dom';
+import './style.css';
+import { Provider } from 'react-redux';
+import store from './store';
+
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <div className="container">
+          <NavBar
+            brandName="MHVillage"
+            contactButton="Contact"
+            searchButton="Search"
+          />
+          <Home />
+          <Footer />
+        </div>
+      </BrowserRouter>
+    </Provider>
+  </StrictMode>
+);
